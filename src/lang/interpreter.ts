370e42f010b00fff2b7eeac4136e6f2f16a3f140
John Hardy <jh@lagado.com>
2025-03-12 22:15:08 +1100

    ATP fixed repl to do multiple commands

--- src/lang/interpreter.ts
+++ src/lang/interpreter.ts
@@ -2,7 +2,9 @@ import { executeOp } from "./ops/builtins";
 import { vm } from "../core/globalState";
 
 export function execute(start: number): void {
+  // vm.debug = true;
   vm.IP = start;
+  vm.running = true;
   while (vm.running) {
     const opcode = vm.next8(); // Read the 8-bit opcode
     if (vm.debug) console.log({ opcode }, vm.IP - 1);
@@ -17,14 +19,6 @@ export function execute(start: number): void {
       throw new Error(errorMessage);
     }
   }
-
-  // After execution, manage memory based on preserve flag
-  if (vm.compiler.preserve) {
-    // Preserve the compiled code: move BP to CP
-    vm.compiler.BP = vm.compiler.CP;
-  } else {
-    // Reuse memory: reset CP to BP
-    vm.compiler.reset();
-  }
+  vm.compiler.reset();
   vm.compiler.preserve = false; // Reset preserve flag
 }
