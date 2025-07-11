1c6bdaebf08daa0904c129d9b8c6e45eca2e408b
John Hardy <jh@lagado.com>
2025-07-11 09:27:56 +0800

    refactor: consolidate stack utilities into single module and remove duplicates

--- src/stack/index.ts
+++ src/stack/index.ts
@@ -1,6 +1,3 @@
-// Core stack utilities used by the VM's core functionality
-export { getStackArgInfo, findTuple as findTupleCore } from './core-utils';
-
 // Operations-specific stack utilities for opcode implementations
 export { findTuple, findElement, rangeRoll } from './ops-utils';
 
